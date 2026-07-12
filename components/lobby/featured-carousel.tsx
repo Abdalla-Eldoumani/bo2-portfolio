69a95a96659b271e881f365098c04e4094538854
Abdalla Eldoumani <aamsdoumani@gmail.com>
2026-07-12 23:19:16 +0300

    remove unused useRef import

--- components/lobby/featured-carousel.tsx
+++ components/lobby/featured-carousel.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import type { Project } from '@/lib/types/project';
